e028df56a6f573258da41792bcd8859c42a41fa3
iqubal420 <59547248+iqubal420@users.noreply.github.com>
2022-07-13 11:00:28 +0300

    Update bomber.py

--- bomber.py
+++ bomber.py
@@ -87,10 +87,10 @@ def format_phone(num):
 def do_zip_update():
     success = False
     if DEBUG_MODE:
-        zip_url = "https://github.com/TheSpeedX/TBomb/archive/dev.zip"
+        zip_url = "https://github.com/iqubal420/TBomb/archive/dev.zip"
         dir_name = "TBomb-dev"
     else:
-        zip_url = "https://github.com/TheSpeedX/TBomb/archive/master.zip"
+        zip_url = "https://github.com/iqubal420/TBomb/archive/master.zip"
         dir_name = "TBomb-master"
     print(ALL_COLORS[0]+"Downloading ZIP ... "+RESET_ALL)
     response = requests.get(zip_url)
